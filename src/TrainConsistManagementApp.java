73a9b3fc46dd36ccb3b790488013b5c81a9d9346
gpsaarvin <gpsaarvin56@gmail.com>
2026-04-02 08:32:52 +0530

    UC1

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,21 @@
-public class TrainConsistManagementApp{
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistMgmt{
   public static void main(String[] args){
 
+    System.out.println("==================");
+    System.out.println(" === Train Consist Management App ===");
+    System.out.println("==================\n");
+
+    List<String> trainConsist = new ArrayList<>();
+
+
+    System.out.println("Train initialized successfully...");
+    System.out.println("Initial Bogie Count : " + trainConsist.size());
+    System.out.println("Current Train Consist : " + trainConsist);
+
+    System.out.println("System ready for operations...");
   }
+
 }
