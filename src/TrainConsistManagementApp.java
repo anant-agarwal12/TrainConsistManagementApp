7de3f22c00f37631a64165e04a8071a7855e7ee4
gpsaarvin <gpsaarvin56@gmail.com>
2026-04-02 08:35:33 +0530

    UC1

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class TrainConsistMgmt{
+public class TrainConsistManagementApp{
   public static void main(String[] args){
 
     System.out.println("==================");
